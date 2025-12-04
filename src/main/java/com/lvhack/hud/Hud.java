6673218844264003e6ce29caf18f1ae9ac4206d4
flyinglv3601 <onlylv3601@gmail.com>
2025-12-04 05:21:39 -0800

    made speed function

--- src/main/java/com/lvhack/hud/Hud.java
+++ src/main/java/com/lvhack/hud/Hud.java
@@ -16,42 +16,55 @@ public class Hud {
 
 
 
-    public static void getPlayerFps(MinecraftClient client, DrawContext context) {
+    public static void getPlayerFps(MinecraftClient client, DrawContext context, int w, int h) {
         int fps = client.getCurrentFps();
         //render text
-        RenderEngineUtil.textRender(context, "fps: " + fps, 5,  10);
+        RenderEngineUtil.textRender(context, "fps: " + fps, 3,  15);
     }
-    public static void getPlayerPosition(MinecraftClient client, DrawContext context) {
+    public static void getPlayerPosition(MinecraftClient client, DrawContext context, int w, int h) {
        double x = (int) client.player.getX();
        double y = (int) client.player.getY();
        double z = (int) client.player.getZ();
 
         //render text
-        RenderEngineUtil.textRender(context, "x: " + x + " y: " + y + "z: " + z, 5,  20);
+        RenderEngineUtil.textRender(context, "x: " + x + " y: " + y + "z: " + z, 3, h - 100);
     }
-    public static void waterMark(MinecraftClient client, DrawContext context) {
+    public static void waterMark(MinecraftClient client, DrawContext context, int w, int h) {
         String clientName = "lvhack";
         //render text
-        RenderEngineUtil.textRender(context, clientName, 5,  30);
+        RenderEngineUtil.textRender(context, clientName, 5,  5);
     }
 
-    public static void welcome(MinecraftClient client, DrawContext context) {
+    public static void welcome(MinecraftClient client, DrawContext context, int w, int h) {
         String userNickName = client.player.getName().getString();
         //render text
-        RenderEngineUtil.textRender(context, "welcone, " + userNickName + "!!!", 5,  40);
+        RenderEngineUtil.drawCenteredText(context, "welcome, " + userNickName + "!!!", w / 2,  3);
     }
-    public static void speed(MinecraftClient client, DrawContext context) {
+    public static void speed(MinecraftClient client, DrawContext context, int w, int h) {
+        double speedX = client.player.getVelocity().x * 20;
+        double speedZ = client.player.getVelocity().z * 20;
+        double speeds = Math.sqrt(speedX * speedX + speedZ * speedZ);
+        String speed = String.format("%.2f", speeds);
+        RenderEngineUtil.textRender(context, "Speed, " + speed, 5,  h - 25);
 
+    }
+    public static void directrion(MinecraftClient client, DrawContext context, int w, int h) {
 
 
     }
 
     public static void hud(DrawContext context){
         MinecraftClient client = MinecraftClient.getInstance();
-        getPlayerFps(client, context);
-        getPlayerPosition(client,context);
-        waterMark(client,context);
-        welcome(client, context);
-        speed(client, context);
+        int width = client.getWindow().getScaledWidth();
+        int height = client.getWindow().getScaledHeight();
+
+
+
+        getPlayerFps(client, context, height, width);
+        getPlayerPosition(client,context, height, width);
+        waterMark(client,context, height, width);
+        welcome(client, context, height, width);
+        speed(client, context, height, width);
+        directrion(client, context, height, width);
     }
 }
--- src/main/java/com/lvhack/utils/RenderEngineUtil.java
+++ src/main/java/com/lvhack/utils/RenderEngineUtil.java
@@ -10,7 +10,8 @@ public class RenderEngineUtil {
         context.drawText(MinecraftClient.getInstance().textRenderer, text, x, y, color, false);
     }
 
-    public static void drawCenteredText(DrawContext context, String text, int x, int y, int color) {
+    public static void drawCenteredText(DrawContext context, String text, int x, int y) {
+        int color = 0xFFB6C1;
         int textWidth = MinecraftClient.getInstance().textRenderer.getWidth(text);
         context.drawText(MinecraftClient.getInstance().textRenderer, text, x - textWidth / 2, y, color, false);
     }
